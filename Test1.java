1ccd8157aa4b64bb999c9fb6374fd231078b2d82
hunglp9397 <lephihung0997@gmail.com>
2024-03-03 00:41:15 +0700

    Update example and answer question

--- Test1.java
+++ Test1.java
@@ -7,13 +7,21 @@ public class Test1 {
      static short d;
 
     public static void main(String[] args) {
-        System.out.println(PI);
+//        System.out.println(PI);
+//
+//       Test1 t = new Test1();
+//       t.a = 1;
+//        System.out.println(t.a);
+//        int a = Integer.valueOf("4");
+//        int b = Integer.parseInt("5");
 
-       Test1 t = new Test1();
-       t.a = 1;
-        System.out.println(t.a);
-        int a = Integer.valueOf("4");
-        int b = Integer.parseInt("5");
+
+        int baseDecimal = 267;
+        int octVal = 0413;
+        int hexVal = 0x10B;
+        int binVal = 0b100001011;
+        System.out.println (baseDecimal + octVal);
+        System.out.println (hexVal + binVal);
     }
 
     public String convert(Integer value){
--- gear/KeyBoard.java
+++ gear/KeyBoard.java
@@ -6,7 +6,7 @@ public class KeyBoard {
 
 
     static void count(){
-
+        long octVal = 0_412;
     };
 
 
@@ -15,6 +15,41 @@ public class KeyBoard {
     void write(){
         count();
         System.out.println(num);
+        long var1 = 0_100_267_760;
+        long var6 = 0x1_0000_10;
+        long var7 = 100__12_12;
+
+        double x = 2.0;
+        float y = 3.0F;
+        double z = x + y;
+
+
+        final byte age1 = 10;
+        final byte age2 = 20;
+        int sum = age1 + age2;
+
+
+        Boolean bool1 = true;
+        Character char1 = 'a';
+        Byte byte1 = 10;
+        Double double1 = 10.98;
+
+        Boolean bool2 = new Boolean(true);
+        Character char2 = new Character('a');
+        Byte byte2 = new Byte((byte)10);
+        Double double2 = new Double(10.98);
+
+        Boolean bool3 = new Boolean("true");
+        Byte byte3 = new Byte("10");
+        Double double3 = new Double("10.98");
+        Boolean bool4 = Boolean.valueOf(true);
+        Boolean bool5 = Boolean.valueOf(true);
+        Boolean bool6 = Boolean.valueOf("TrUE");
+        Double double4 = Double.valueOf(10);
+
+
+
+
     };
 
 
