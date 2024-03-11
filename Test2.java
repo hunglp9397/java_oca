f909caba34b92b233fd9ef84a78f5580b95f89a3
hunglp9397 <lephihung0997@gmail.com>
2024-03-12 01:06:09 +0700

    update example java

--- Test2.java
+++ Test2.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import java.util.stream.Stream;
 
@@ -29,10 +31,38 @@ public final class Test2 {
 //         System.out.println(Arrays.binarySearch(numbers, 1)); // -1
 //        System.out.println(Arrays.binarySearch(numbers, 3)); // -2
 //        System.out.println(Arrays.binarySearch(numbers, 9)); // -5
+//
 
-         int[] numbers = new int[] {3,2,1};
-         System.out.println(Arrays.binarySearch(numbers, 2));
-         System.out.println(Arrays.binarySearch(numbers, 3));
+//         int[] numbers = new int[] {3,2,1};
+//         System.out.println(Arrays.binarySearch(numbers, 2));
+//         System.out.println(Arrays.binarySearch(numbers, 3));
+//
+//        ArrayList list = new ArrayList();
+//        list.add("hello");
+//        list.add(Boolean.TRUE);
+//        list.forEach(System.out::println);
+
+//        ArrayList<String>  list = new ArrayList<>();
+//        list.add("1");
+//        list.add(Boolean.TRUE);
+
+//        List<Double> weights = Arrays.asList(60.0, 50.0);
+//        double first = weights.get(0);
+//        double second = weights.get(1);
+//        System.out.println(first);
+//        System.out.println(second);
+
+
+        List<String> listStr = new ArrayList<>();
+        listStr.add("a");
+        listStr.add("b");
+        String [ ] strArr = listStr.toArray(new String [0]);
+        System.out.println(strArr.length);
+        Stream.of(strArr).forEach(System.out::println);
+
+
+
+
 
 
 
