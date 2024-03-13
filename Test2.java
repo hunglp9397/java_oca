a7bcd31b7255bc08daebe85443c0cc2fbf92c654
hunglp9397 <lephihung0997@gmail.com>
2024-03-14 02:35:42 +0700

    update example java

--- Test2.java
+++ Test2.java
@@ -1,5 +1,9 @@
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 
 public final class Test2 {
     String s;
@@ -79,10 +83,36 @@ public final class Test2 {
 
 
 
-        LocalDate start = LocalDate.of(2015, Month.JANUARY, 1);
-        LocalDate end = LocalDate.of(2015, Month.MARCH, 30);
+//        LocalDate start = LocalDate.of(2015, Month.JANUARY, 1);
+//        LocalDate end = LocalDate.of(2015, Month.MARCH, 30);
+//
+//        perform(start, end);
 
-        perform(start, end);
+//
+//        LocalDate date = LocalDate.of(2020, Month.JANUARY, 20);
+//        LocalTime time = LocalTime.of(11, 12, 34);
+//        LocalDateTime dateTime = LocalDateTime.of(date, time);
+
+//        System.out.println(date);
+//        System.out.println(time);
+//        System.out.println(dateTime);
+//
+//
+//        System.out.println(date.format(DateTimeFormatter.ISO_LOCAL_DATE));
+//        System.out.println(time.format(DateTimeFormatter.ISO_LOCAL_TIME));
+//        System.out.println(dateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+
+//        DateTimeFormatter shortDateTime =  DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
+//        System.out.println(shortDateTime.format(dateTime)); // 1/20/20
+//        System.out.println(shortDateTime.format(date)); // 1/20/20
+//        System.out.println(shortDateTime.format(time)); // UnsupportedTemporalTypeException
+
+
+        DateTimeFormatter formater = DateTimeFormatter.ofPattern("MM dd yyyy");
+        LocalDate date = LocalDate.parse("01 02 2015", formater);
+        LocalTime time = LocalTime.parse("11:22");
+        System.out.println(date); // 2015-01-02
+        System.out.println(time); // 11:22
 
     }
 
