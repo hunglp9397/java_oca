65f807251f9c6d3945add50c6cff2b490f0c47ee
hunglp9397 <lephihung0997@gmail.com>
2024-03-24 09:04:08 +0700

    update example java

--- building/Person.java
+++ building/Person.java
@@ -1,7 +1,9 @@
 package building;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.*;
 
 public class Person {
     String name;
@@ -11,17 +13,106 @@ public class Person {
     }
 
     public static void main(String[] args) {
-        StringBuilder sb1 = new StringBuilder();
-        sb1.append("Java");
-        sb1.append(new Person("Oracle"));
-        System.out.println(sb1);
+//        StringBuilder sb1 = new StringBuilder();
+//        sb1.append("Java");
+//        sb1.append(new Person("Oracle"));
+//        System.out.println(sb1);
 
 //        LocalTime time = LocalTime.MAX;
 //        System.out.println(time);
 
-        LocalDate localDate = LocalDate.of(2015,02,23);
-        localDate = localDate.plusDays(1);
-        System.out.println(localDate);
+//        LocalDate localDate = LocalDate.of(2015,02,23);
+//        localDate = localDate.plusDays(1);
+//        System.out.println(localDate);
+//
+//        String s = "2";
+//        s += 'x';
+//        s += false;
+//        System.out.println(s);
+//
+//        java.util.Date [] dates [] = new java.util.Date[2][];
+//
+//
+//        ArrayList l = new ArrayList();
+//        int num = l.size();
+//
+//        List<String> hex = Arrays.asList("30", "8", "3A", "FF");
+//        Collections.sort(hex);
+//
+//
+//        List<String> one = new ArrayList<>();
+//        List<String> two = new ArrayList<>();
+//
+//        System.out.println(one.equals(two));
+//
+//        LocalDate date = LocalDate.parse("2018-04-30", DateTimeFormatter.ISO_LOCAL_DATE);
+//        System.out.println( date);
+//
+//        LocalDate date1 = LocalDate.of(2018, Calendar.APRIL, 40);
+//        System.out.println(date1.getDayOfMonth());
+
+//        LocalDateTime dt = LocalDateTime.of(2015, 5, 10, 11, 22,33);
+//        Period p = Period.of(1,2,3);
+//        dt = dt.minus(p);
+//        System.out.println(dt);
+//        DateTimeFormatter f = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT);
+//        System.out.println(dt.format(f));
+//
+//
+//        char [] c = new char[2];
+//        int lenght = c.length;
+//
+//        ArrayList<Integer> arr = new ArrayList<>();
+//        System.out.println(arr.size());
+
+//
+//        int[] random = { 6, -4, 12, 0, -10 };
+//        int x = 12;
+//        int y = Arrays.binarySearch(random, x);
+//        System.out.println(y);
+//
+//        String [] names = {};
+//        List<String> list = Arrays.asList(names);
+
+//
+//        List<String> one = new ArrayList<String>();
+//        one.add("abc");
+//        List<String> two = new ArrayList<>();
+//        two.add("abc");
+//        if (one == two)
+//            System.out.println("A");
+//        else if (one.equals(two))
+//            System.out.println("B");
+//        else
+//            System.out.println("C");
+//        List<Integer> nums = new LinkedList<>();
+//        List<Integer> nums2 = new LinkedList<>();
+//        System.out.println(nums2.equals(nums));
+//       LocalDate d1 = LocalDate.of(2014, 6, 21);
+//        LocalDate d2 =LocalDate.of(2014, Calendar.JUNE, 21);
+//        LocalDate d3 =LocalDate.of(2014, Month.JUNE, 21);
+//
+//        System.out.println(d1);
+//        System.out.println(d2);
+//        System.out.println(d3);
+
+//        LocalDate date = LocalDate.of(2018, Month.APRIL, 30);
+//        date.plusDays(2);
+//        date.plusYears(3);
+//        System.out.println(date.getYear() + " " + date.getMonth() + " "
+//                + date.getDayOfMonth());
+
+
+//        LocalDateTime d = LocalDateTime.of(2015, 5, 10, 11, 22, 33);
+//        Period p = Period.ofDays(1).ofYears(2);
+//        d = d.minus(p);
+//        DateTimeFormatter f = DateTimeFormatter.ofLocalizedDateTime(FormatStyle
+//                .SHORT);
+//        System.out.println(f.format(d));
+
+        int[] nums = new int []{1,2,3};
+        System.out.println(Arrays.binarySearch(nums,2));
+        System.out.println(Arrays.binarySearch(nums,3));
 
     }
 }
