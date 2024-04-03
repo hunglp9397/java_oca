e3fa747cdc2bfee44c5c42425c5e78d77e9eb236
hunglp9397 <lephihung0997@gmail.com>
2024-04-03 23:40:31 +0700

    update example java

---
+++ package_3/Forest.java
@@ -0,0 +1,9 @@
+package package_3;
+
+public class Forest {
+    public long treeCount;
+
+    public Forest(long treeCount){
+        this.treeCount = treeCount;
+    }
+}
---
+++ package_3/RainForest.java
@@ -0,0 +1,29 @@
+package package_3;
+
+import building.Inte;
+
+import java.util.ArrayList;
+
+public class RainForest  {
+
+
+
+
+
+
+
+
+    public Integer exampleReturn(int intValue){
+        return intValue;
+    }
+    public static void main(String[] args) {
+        char c = 10;
+        System.out.println(new RainForest().exampleReturn(c));
+    }
+
+
+
+
+
+
+}
