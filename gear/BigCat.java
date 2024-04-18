b2c5498ddcbe2955a456e5bdf274ed047bb9c914
hunglp9397 <lephihung0997@gmail.com>
2024-04-18 15:21:04 +0700

    update example java

--- gear/BigCat.java
+++ gear/BigCat.java
@@ -1,15 +1,25 @@
 package gear;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BigCat {
     public static void main(String[] args) {
-     foo();
+        System.out.println(new StringBuilder("cl").insert(2,"own"));
+
+        StringBuilder builder = new StringBuilder("12");
+        builder.reverse();
+        System.out.println(builder);
     }
+
 
     static void foo() throws RuntimeException{
 
         double k = 1;
         double x = 1.0f;
         float y = 1.0F;
+
+        List a = new ArrayList();
 
     }
 }
---
+++ gear/Shoot.java
@@ -0,0 +1,21 @@
+package gear;
+
+import java.util.function.Predicate;
+
+public class Shoot {
+
+    interface Target {
+        boolean need(double angle);
+    }
+
+    static void prepare(double angle, Target t){
+
+
+        StringBuilder m = new StringBuilder();
+        char c = m.charAt(3);
+
+    public static void main(String[] args) {
+
+    }
+
+}
