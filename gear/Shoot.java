20b756599117421d4d1d13142375c9e4fc9267cf
hunglp9397 <lephihung0997@gmail.com>
2024-04-18 22:42:09 +0700

    update example java

--- gear/Shoot.java
+++ gear/Shoot.java
@@ -1,8 +1,16 @@
 package gear;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.function.Predicate;
 
 public class Shoot {
+
+    Date d = null;
+
+    static String s= "";
 
     interface Target {
         boolean need(double angle);
@@ -10,12 +18,28 @@ public class Shoot {
 
     static void prepare(double angle, Target t){
 
+        StringBuilder builder = new StringBuilder("0123456");
+        builder.sub
+
+
+        StringBuilder builder = new StringBuilder();
+
+        System.out.println(s.length());
+        System.out.println(builder.length());
+
 
         StringBuilder m = new StringBuilder();
         char c = m.charAt(3);
 
-    public static void main(String[] args) {
 
+        List<String> s = new ArrayList<>();
+        System.out.println(s.size());
+
+
+
+        Predicate predicate_1 = item -> item.equals("s");
+
+        Predicate predicate_2 = p -> p.toString().startsWith("abc");
     }
 
 }
