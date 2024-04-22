190281d441cb76119e7aee150dc0bdee8b9f97e7
hunglp9397 <lephihung0997@gmail.com>
2024-04-23 06:18:25 +0700

    update example java

---
+++ Sphere.java
@@ -0,0 +1,19 @@
+import java.util.function.Predicate;
+
+public abstract  class Sphere {
+
+    abstract  void foo();
+
+    static final void a(){
+
+    }
+
+    void secret(String mystery){
+        mystery = mystery.replace("1","8");
+        mystery.startsWith("");
+        String s= mystery.toString();
+
+        Predicate<String> pred = (String  o) -> true;
+        Predicate dash = c -> c.s
+    }
+}
--- Tools.java
+++ Tools.java
@@ -2,25 +2,6 @@ public class Tools {
     private static int yesterday = 1;
     int tomorrow = 10;
 
-    public final int static
 
-    public static void main(String[] args) {
-        int f = 1;
-        switch (f){
-            case  1:
-                System.out.println("10");
-            case 20:
-                System.out.println("20");
-            default:
-                System.out.println("default");
-        }
 
-        String s [] = new String[1];
-        System.out.println(s.length);
-        System.out.println();
-        String abc = "";
-        StringBuilder sb = new StringBuilder();
-        System.out.println(sb.length());
-        System.out.println(abc.length());
-    }
 }
