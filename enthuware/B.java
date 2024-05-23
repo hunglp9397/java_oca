eaf8aa6ace79dd505b12055eb9a9efaa309bb34b
hunglp9397 <lephihung0997@gmail.com>
2024-05-24 01:42:10 +0700

    update example java

--- enthuware/B.java
+++ enthuware/B.java
@@ -1,17 +1,26 @@
 package enthuware;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class B extends A{
 
 
+
     int i = 4;
 
     public static void main(String[] args) {
-            A a = new B();
-            a.print();
-            float f = 0x0123;
+           List s1 = new ArrayList<>();
+           try {
+               while(true){
+                   s1.add("a");
+               }
+           }catch (RuntimeException e){
+               e.printStackTrace();
+           }
+        System.out.println(s1.size());
     }
 
     void print(){
--- enthuware/Test.java
+++ enthuware/Test.java
@@ -1,5 +1,8 @@
 package enthuware;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Test {
 
     public OtherClass oc = new OtherClass();
@@ -23,16 +26,56 @@ public class Test {
     }
 
 
+    static void cal(long a){
 
- int main;
-
+    }
 
     public static void main(String[] args) {
 
-//        String s = (String) new Object();
-//        System.out.println(s);
 
-        System.out.println(4 + 1.0f);
+//        int intValue = 1;
+//        short shortValue = 1;
+//        char charValue = 'c';
+//        byte byteValue = 0;
+//
+//        cal(byteValue);
+//        cal(shortValue);
+//        cal(charValue);
+//        cal(intValue);
+
+
+//        ArrayList<Double> a  = new ArrayList<>();
+////        System.out.println(a.contains("string"));
+//
+//        System.out.println(a.contains(true));
+//        System.out.println(a.contains('x'));
+//        System.out.println(a.contains(new LinkedList<>()));
+//        boolean b = 5> 0;
+
+//        String a = "abc";
+//        short s = 1;
+//        char c = a.charAt('0');
+
+
+
+//        int i = 1;
+//        int j = i++;
+//        if ( (i == ++j) | (i++ == j)){
+//            i += j;
+//        }
+//        System.out.println(i);
+
+
+        try {
+            return;
+        } catch (Exception e) {
+            return;
+        } finally {
+            System.out.println("The end!");
+        }
+
+
+
     }
 
     static void print(double a){
