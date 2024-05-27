954fb572c0aac041246b7f91c2336bc778bc8bc0
hunglp9397 <lephihung0997@gmail.com>
2024-05-27 08:05:53 +0700

    update example java

---
+++ castles/inheritance/Space.java
@@ -0,0 +1,12 @@
+package castles.inheritance;
+
+public class Space {
+    public static void main(String[] args) {
+
+        System.out.println(new Space().foo());
+    }
+
+    void foo(){
+
+    }
+}
