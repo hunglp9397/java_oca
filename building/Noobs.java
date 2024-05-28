ac34fe593eac29f20e87f6f68817acf09998beae
hunglp9397 <lephihung0997@gmail.com>
2024-05-28 23:27:06 +0700

    update example java

---
+++ building/Noobs.java
@@ -0,0 +1,17 @@
+package building;
+
+public class Noobs {
+
+    void m(int a){
+
+    }
+
+    void m(char c){
+
+    }
+
+    public static void main(String[] args) {
+        char c1 = 'u';
+        System.out.println(c1);
+    }
+}
--- building/Sub1.java
+++ building/Sub1.java
@@ -4,7 +4,10 @@ import castles.enthuware5.Sub;
 
 public class Sub1 extends Super{
     public static void main(String[] args) {
-        System.out.println(Sub1.id);
-       Sub1.foo();
+       final byte age1 = 1;
+       final byte age2 = 2;
+
+       final int sum = age2 + age1;
+       final byte sumByte = age1 + age2;
     }
 }
